84b0e0f7f0e153f808485df7aad455892f18b170
PatrickDohn <dohnpatrick1993@gmail.com>
2020-09-22 12:33:44 -0400

    deletes channels

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -75,8 +75,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
-    'channels'
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
